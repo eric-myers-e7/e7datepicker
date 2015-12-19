08cf8283240ece28fd065d1f113d6a3f11e8765b
Eric Myers <eric.myers@e7solutions.com>
2015-12-19 00:31:54 -0500

    added ngannotate

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,17 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
+    ngAnnotate: {
+       options: {
+           singlequotes: true
+       },
+        app: {
+            files: {
+              'src/ng-datepicker.ann.js': ['src/ng-datepicker.js'],
+              'src/template.ann.js': ['src/template.js']
+            }
+        }
+    },
     uglify: {
        options: {
           preserveComments: 'some',
@@ -13,14 +23,15 @@ module.exports = function(grunt) {
        },
        dist: {
           files: {
-             'dist/ng-datepicker.min.js': ['src/ng-datepicker.js'],
-             'dist/template.min.js': ['src/template.js']
+             'dist/ng-datepicker.min.js': ['src/ng-datepicker.ann.js'],
+             'dist/template.min.js': ['src/template.ann.js']
           }
        }
      }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-ng-annotate');
 
-  grunt.registerTask('prep', ['uglify']);
+  grunt.registerTask('prep', ['ngAnnotate', 'uglify']);
 };
